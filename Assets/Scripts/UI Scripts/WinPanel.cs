214bae171e2863a10c85b36eb2d51c057682bd1a
ilia <ilya@1-tn.com>
2020-08-19 19:23:01 +0200

    added new script WinPanel

---
+++ Assets/Scripts/UI Scripts/WinPanel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class WinPanel : MonoBehaviour
+{
+    [Header("Level Information")]
+    public string levelToLoad;
+    public int level;
+    private GameData gameData;
+    private int starsActive;
+    private int highScore;
+
+
+    [Header("UI stuff")]
+    public Image[] stars;
+    public Text highScoreText;
+    public Text starText;
+    void OnEnable()
+    {
+        gameData = FindObjectOfType<GameData>();
+        LoadData();
+        ActivateStars();
+        SetText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
